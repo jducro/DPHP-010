3a835d2d6d9ba79d914923b927c05ca03bea9f0b
Julien Ducro <julien@chictypes.com>
2013-12-09 09:56:50 +0100

    Traitement du formulaire.

---
+++ contact.php
@@ -0,0 +1,15 @@
+<section id="contact">
+	<article>
+		<fieldset>
+			<legend>Contact</legend>
+			<form action="index.php?page=contact" method="post">
+				<p><label for="prenom">Prénom</label><input name="prenom" id="prenom" type="text"/></p>
+				<p><label for="nom">Nom</label><input name="nom" id="nom" type="text"/></p>
+				<p><label for="email">Email</label><input name="email" id="email" type="email"/></p>
+				<p><label for="phone">Téléphone</label><input name="phone" id="phone" type="tel"/></p>
+				<p><label for="question">Question</label><textarea name="question" id="question"></textarea></p>
+				<p><input type="submit" value="Envoyer" name="contact" /></p>
+			</form>
+		</fieldset>
+	</article>
+</section>
--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
+if (!empty($_POST)) {
+	$message = $_POST['question'];
+	$email = $_POST['email'];
+	mail('ducro_j@etna-alternance.net', 'Message depuis le blog par '.$_POST['prenom'].' '.$_POST['nom'], $message);
+}
 ob_start();
 if (empty($_GET['page']) || $_GET['page'] == 'index') {
 	include_once('accueil.php');
 } elseif ($_GET['page'] == 'a_propos') {
 	include_once('a_propos.php');
+} elseif ($_GET['page'] == 'contact') {
+	include_once('contact.php');
 }
 $content = ob_get_contents();
 ob_clean();
